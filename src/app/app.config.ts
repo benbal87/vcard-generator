eeb9b228fc37df8047a6b8504aed461e9b8250ca
Benedek Balázs <benbal87@gmail.com>
2023-12-27 16:00:28 +0700

    :heavy_plus_sign: dependencies installed & package.json start command updated
    - angular material
    - zod
    - vcf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  BrowserAnimationsModule,
+  provideAnimations
+} from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom([BrowserAnimationsModule, BrowserModule, HttpClientModule])
+  ]
 };
